bee51b743ad65ca8cdbf5ac75c7b8b51db651f10
manojbangari <59486302+manojbangari@users.noreply.github.com>
2025-11-16 12:48:30 +0530

    Update eks.tf

--- module/eks.tf
+++ module/eks.tf
@@ -32,18 +32,21 @@ resource "aws_iam_openid_connect_provider" "eks-oidc" {
 }
 
 
-# AddOns for EKS Cluster
 resource "aws_eks_addon" "eks-addons" {
   for_each      = { for idx, addon in var.addons : idx => addon }
   cluster_name  = aws_eks_cluster.eks[0].name
   addon_name    = each.value.name
   addon_version = each.value.version
 
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
   depends_on = [
     aws_eks_node_group.ondemand-node,
     aws_eks_node_group.spot-node
   ]
 }
+
 
 # NodeGroups
 resource "aws_eks_node_group" "ondemand-node" {
